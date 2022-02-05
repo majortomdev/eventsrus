6a16aaf025c93ce9629546956bb686935f2d446e
Joseph Kinahan <kinahanj25@gmail.com>
2022-02-05 12:46:56 +0000

    initial commit

---
+++ src/main/java/com/jksports/eventsrus/EventsrusApplication.java
@@ -0,0 +1,13 @@
+package com.jksports.eventsrus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsrusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsrusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jksports/eventsrus/EventsrusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jksports.eventsrus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsrusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
